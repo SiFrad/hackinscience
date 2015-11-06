4828dea5dd4c40cace8ab191b162fdb59aeff4bd
SiFrad <simon.fradet@cri-paris.org>
2015-11-06 17:56:33 +0100

    essai2

--- exercises/008/solution.py
+++ exercises/008/solution.py
@@ -1,3 +1,3 @@
-liste=['H','e','l','l','o',' ','W','o','r','l','d',' ','!']
-for c in liste :
-    print (c)
+liste = ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', ' ', '!']
+for c in liste:
+    print(c)
