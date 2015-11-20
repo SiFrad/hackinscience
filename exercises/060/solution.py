85ffe00a133113a6c2d379a75c7133eabc33b94b
SiFrad <simon.fradet@cri-paris.org>
2015-11-20 17:26:54 +0100

    e

--- exercises/060/solution.py
+++ exercises/060/solution.py
@@ -1,5 +1,5 @@
-f = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',\
- 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+import string
+f = list(string.ascii_lowercase)
 for i in f:
     for j in f:
         print(i + j)
