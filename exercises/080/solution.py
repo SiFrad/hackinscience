c196440c1141c78d76a1c08a888caaa77b71a0c5
SiFrad <simon.fradet@cri-paris.org>
2015-11-20 18:58:19 +0100

    d

--- exercises/080/solution.py
+++ exercises/080/solution.py
@@ -1,7 +1,6 @@
 import string
 f = "abcdefghijklmnopqrstuvwxyz"
-for i in range(0,26):
+for i in range(0, 26):
     for j in range(i+1, 26):
         if i != j:
             print(f[i] + f[j])
-    
