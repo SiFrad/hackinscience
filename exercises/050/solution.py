45bbd7c698383c6c9e99d742cb2a60e59bf1897a
SiFrad <simon.fradet@cri-paris.org>
2015-11-20 16:45:47 +0100

    essai2

--- exercises/050/solution.py
+++ exercises/050/solution.py
@@ -1,9 +1,9 @@
 f = range(0, 1001)
 s = 0
 for i in f:
-    if i % 3 == 0:
+    if i % 3 == 0 and i % 5 != 0:
         s = s + i
-    for j in f:
-        if j % 5 == 0 and j % 3 != 0:
-            s = s + j
+for j in f:
+    if j % 5 == 0 and j % 3 != 0:
+        s = s + j
 print(s)
